6ab6544c5e9f1b7168bb4787e0ee64b8d9a84701
Jwahoon <catnofat@gmail.com>
2023-09-27 12:47:33 +0900

    feat: 뱃지 컴포넌트 구현
    - 피그마 기획했던 파일 크기와 가장 비슷한 크기를 선택하여 구현하였음

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Badge from "./components/atoms/Badge/Badge";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Badge />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
