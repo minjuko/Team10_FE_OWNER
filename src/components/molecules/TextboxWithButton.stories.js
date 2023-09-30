8501ae07c3652a372714a5a01f9a7f00713be66f
catnofat <catnofat@gmail.com>
2023-09-30 17:38:33 +0900

    feat: molecule의 TextboxWithButton 컴포넌트 작성
    TextboxWithButton 하위 컴포넌트인 Button, TextInput 수정
    TextInput : const getType 추가 작성, 스토리북에 withbutton case추가
    Button: 스토리북에 withTextBox case 추가

---
+++ src/components/molecules/TextboxWithButton.stories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import TextboxWithButton from "./TextboxWithButton";
+
+export default {
+  title: "molecules/TextboxWithButton",
+  component: TextboxWithButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    type: "text",
+    placeholder: { control: "text" },
+  },
+};
+
+export const MainTextWithButton = {
+  args: {
+    buttonlabel: "주소찾기",
+    placeholder: { control: "text" },
+  },
+};
