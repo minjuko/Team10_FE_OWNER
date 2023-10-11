f9e6a630a85a0e516cfc807007bb0423609db1e0
Juyeong "James" Noh <104883910+juyeongnoh@users.noreply.github.com>
2023-10-11 05:04:01 +0000

    Revert "Revert "전남대_10조_뽀득뽀득_4주차""

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const getType = (type) => {
+  switch (type) {
+    case "long":
+      return "block w-96 h-14 p-4 bg-sky-500 text-white font-semibold rounded-none";
+    case "longwhite":
+      return "block w-96 h-14 p-4 bg-white text-black font-semibold rounded-none";
+    case "small":
+      return "block w-24 h-14 p-4 bg-gray-600 text-white font-semibold rounded-none";
+    case "addPhoto":
+      return "block w-16 h-16 p-4 bg-gray-100 text-gray-600 font-semibold rounded-none";
+    case "withTextInput":
+      return "w-28 h-14 px-4 rounded-xl bg-blue-100 text-blue-500";
+  }
+};
+
+export const Button = ({ type, label, ...props }) => {
+  return (
+    <button
+      type={type}
+      label={label}
+      className={`${getType(type)} `}
+      {...props}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {label}
+    </button>
+  );
+};
