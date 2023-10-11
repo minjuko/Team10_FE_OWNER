239463eafc230c5268061e2b641580b443fabc25
juyeongnoh <juyeongnoh@gmail.com>
2023-10-11 22:09:12 +0900

    design: DashboardItem w-screen에서 w-96으로 수정

--- src/components/atoms/DashboardItem.jsx
+++ src/components/atoms/DashboardItem.jsx
@@ -2,7 +2,7 @@ import Photo from "./Photo";
 
 const DashboardItem = ({ label, number, percentage, icon }) => {
   return (
-    <div className="w-screen flex items-center justify-between p-4 bg-white rounded-xl">
+    <div className="w-96 flex items-center justify-between p-4 bg-white rounded-xl">
       <div>
         <div>{label}</div>
         <div className="text-3xl font-bold">{number}</div>
