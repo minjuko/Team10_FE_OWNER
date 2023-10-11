6e50d9b2e01f1454348ebf1005cbf8238b753e28
Juyeong "James" Noh <104883910+juyeongnoh@users.noreply.github.com>
2023-10-11 04:26:42 +0000

    Revert "전남대_10조_뽀득뽀득_4주차"

--- src/services/users.js
+++
@@ -1,9 +0,0 @@
-import { instance } from "./index";
-
-export const login = (data) => {
-  const { email, password } = data;
-  return instance.post("/login/owner", {
-    email,
-    password,
-  });
-};
