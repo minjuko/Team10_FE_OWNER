cf3299fcc82e9bbe130757c48c083caae1b77390
juyeongnoh <juyeongnoh@gmail.com>
2023-10-13 21:49:47 +0900

    design: SalesItem w-auto 옵션 추가

--- src/components/organisms/SalesItem.jsx
+++ src/components/organisms/SalesItem.jsx
@@ -6,13 +6,13 @@ import ProfileIcon from "/profile.svg";
  * SalesItem 컴포넌트
  * 개별 판매내역을 보여주는 컴포넌트입니다.
  *
- * @param {reservationId} 예약번호
- * @param {carwashName} 세차장 이름
- * @param {bayNo} 베이 번호
- * @param {nickname} 고객 닉네임
- * @param {totalPrice} 총 결제금액
- * @param {startTime} 시작시간
- * @param {endTime} 종료시간
+ * @param {String} reservationId 예약번호
+ * @param {String} carwashName 세차장 이름
+ * @param {String} bayNo 베이 번호
+ * @param {String} nickname 고객 닉네임
+ * @param {String} totalPrice 총 결제금액
+ * @param {String} startTime 시작시간
+ * @param {String} endTime 종료시간
  */
 
 const SalesItem = ({
@@ -25,7 +25,7 @@ const SalesItem = ({
   endTime,
 }) => {
   return (
-    <Box className="p-4 grid gap-4">
+    <Box className="w-auto p-4 grid gap-4">
       <div className="text-xl text-gray-400">#{reservationId}</div>
       <div className="flex justify-between">
         <div className="text-xl">
