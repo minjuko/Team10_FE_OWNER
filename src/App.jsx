f3b627ee9b51982eb07f0e1209b93a52c791f4d9
juyeongnoh <juyeongnoh@gmail.com>
2023-10-16 15:05:19 +0900

    chore: App.jsx /manage/item Route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import SalesManagementPage from "./pages/SalesManagementPage";
 import MainLayout from "./layouts/MainLayout";
+import CarwashManagementPage from "./pages/CarwashManagementPage";
+import CarwashItemManagementPage from "./pages/CarwashItemManagementPage";
 
 const App = () => {
   return (
@@ -14,6 +16,10 @@ const App = () => {
           <Route element={<MainLayout />}>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/sales" element={<SalesManagementPage />}></Route>
+            <Route path="/manage" element={<CarwashManagementPage />}></Route>
+            <Route
+              path="/manage/item"
+              element={<CarwashItemManagementPage />}></Route>
           </Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/signup" element={<SignupPage />}></Route>
