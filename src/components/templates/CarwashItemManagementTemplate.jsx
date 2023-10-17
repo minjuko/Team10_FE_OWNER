c927f2bccd6e4fa652359e56d6833ff51ce79bb9
juyeongnoh <juyeongnoh@gmail.com>
2023-10-17 14:35:17 +0900

    chore: CarwashItemManagementTemplate response에 start_time, end_time 추가

--- src/components/templates/CarwashItemManagementTemplate.jsx
+++ src/components/templates/CarwashItemManagementTemplate.jsx
@@ -9,17 +9,37 @@ const CarwashItemManagementTemplate = ({
     star: 4.3,
     monthly_sales_by_store: 450000,
     monthly_reservation_by_store: 45,
+    start_time: "2023-10-16T08:00",
+    end_time: "2023-10-17T02:00",
     bay_list: [
       {
         bay_no: 8,
         bay_bookedTime: [
+          // 전날 오후 11시 ~ 오늘 오전 2시
           {
-            start_time: "2021-09-01 19:00:00",
-            end_time: "2021-09-01 20:00:00",
+            start_time: "2023-10-15T23:00:00",
+            end_time: "2023-10-16T02:00:00",
           },
           {
-            start_time: "2021-09-01 20:00:00",
-            end_time: "2021-09-01 21:00:00",
+            start_time: "2023-10-16T07:00:00",
+            end_time: "2023-10-16T09:00:00",
+          },
+          {
+            start_time: "2023-10-16T04:00:00",
+            end_time: "2023-10-16T06:00:00",
+          },
+          {
+            start_time: "2023-10-16T20:00:00",
+            end_time: "2023-10-16T21:00:00",
+          },
+          {
+            start_time: "2023-10-16T19:00:00",
+            end_time: "2023-10-16T20:00:00",
+          },
+          // 오늘 오후 11시 ~ 다음날 오전 1시 30분
+          {
+            start_time: "2023-10-16T23:00:00",
+            end_time: "2023-10-17T01:30:00",
           },
         ],
       },
@@ -27,12 +47,12 @@ const CarwashItemManagementTemplate = ({
         bay_no: 9,
         bay_bookedTime: [
           {
-            start_time: "2021-09-01 19:00:00",
-            end_time: "2021-09-01 20:00:00",
+            start_time: "2023-10-16T19:00:00",
+            end_time: "2023-10-16T20:00:00",
           },
           {
-            start_time: "2021-09-01 20:00:00",
-            end_time: "2021-09-01 21:00:00",
+            start_time: "2023-10-16T20:00:00",
+            end_time: "2023-10-16T21:00:00",
           },
         ],
       },
@@ -71,7 +91,14 @@ const CarwashItemManagementTemplate = ({
       </aside>
       <section className="grid flex-grow gap-4">
         {carwash.bay_list.map((bay) => {
-          return <CarwashBayItem key={bay.bay_no} bay={bay} />;
+          return (
+            <CarwashBayItem
+              key={bay.bay_no}
+              start_time={carwash.start_time}
+              end_time={carwash.end_time}
+              bay={bay}
+            />
+          );
         })}
       </section>
     </div>
