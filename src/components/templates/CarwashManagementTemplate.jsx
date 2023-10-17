4f94ab7a92a3611f759d1ce673bcf06ae86869a2
juyeongnoh <juyeongnoh@gmail.com>
2023-10-17 15:51:08 +0900

    feat: MultipleTimeTable 컴포넌트 구현
    CarwashItem 제목 아래에 MultipleTimeTable이 표시될 수 있게 Template과 Page도 수정하였습니다.
    
    resolves: #47

--- src/components/templates/CarwashManagementTemplate.jsx
+++ src/components/templates/CarwashManagementTemplate.jsx
@@ -8,19 +8,37 @@ const CarwashManagementTemplate = ({
     {
       image: CarwashImage,
       name: "용봉세차타운",
-      start_time: "1970-01-01T07:00",
-      end_time: "1970-01-01T22:00",
+      start_time: "2023-10-17T00:00",
+      end_time: "2023-10-17T23:59",
       bay_list: [
         {
           bay_no: 8,
           bay_bookedTime: [
+            // 전날 오후 11시 ~ 오늘 오전 2시
             {
-              start_time: "2021-09-01 19:00:00",
-              end_time: "2021-09-01 20:00:00",
+              start_time: "2023-10-15T23:00:00",
+              end_time: "2023-10-17T02:00:00",
             },
             {
-              start_time: "2021-09-01 20:00:00",
-              end_time: "2021-09-01 21:00:00",
+              start_time: "2023-10-17T07:00:00",
+              end_time: "2023-10-17T09:00:00",
+            },
+            {
+              start_time: "2023-10-17T04:00:00",
+              end_time: "2023-10-17T06:00:00",
+            },
+            {
+              start_time: "2023-10-17T20:00:00",
+              end_time: "2023-10-17T21:00:00",
+            },
+            {
+              start_time: "2023-10-17T19:00:00",
+              end_time: "2023-10-17T20:00:00",
+            },
+            // 오늘 오후 11시 ~ 다음날 오전 1시 30분
+            {
+              start_time: "2023-10-17T23:00:00",
+              end_time: "2023-10-18T01:30:00",
             },
           ],
         },
@@ -28,12 +46,25 @@ const CarwashManagementTemplate = ({
           bay_no: 9,
           bay_bookedTime: [
             {
-              start_time: "2021-09-01 19:00:00",
-              end_time: "2021-09-01 20:00:00",
+              start_time: "2023-10-17T05:00:00",
+              end_time: "2023-10-17T08:00:00",
             },
             {
-              start_time: "2021-09-01 20:00:00",
-              end_time: "2021-09-01 21:00:00",
+              start_time: "2023-10-17T12:00:00",
+              end_time: "2023-10-17T13:30:00",
+            },
+          ],
+        },
+        {
+          bay_no: 10,
+          bay_bookedTime: [
+            {
+              start_time: "2023-10-17T01:00",
+              end_time: "2023-10-17T02:30",
+            },
+            {
+              start_time: "2023-10-17T05:30",
+              end_time: "2023-10-17T06:30",
             },
           ],
         },
@@ -42,19 +73,19 @@ const CarwashManagementTemplate = ({
     {
       image: CarwashImage,
       name: "수완세차타운",
-      start_time: "1970-01-01T07:00",
-      end_time: "1970-01-01T22:00",
+      start_time: "2023-10-17T07:00",
+      end_time: "2023-10-17T22:00",
       bay_list: [
         {
           bay_no: 4,
           bay_bookedTime: [
             {
-              start_time: "2021-09-01 19:00:00",
-              end_time: "2021-09-01 20:00:00",
+              start_time: "2023-10-17T07:00",
+              end_time: "2023-10-17T11:00",
             },
             {
-              start_time: "2021-09-01 20:00:00",
-              end_time: "2021-09-01 21:00:00",
+              start_time: "2023-10-17T15:00",
+              end_time: "2023-10-17T18:00",
             },
           ],
         },
@@ -62,12 +93,12 @@ const CarwashManagementTemplate = ({
           bay_no: 5,
           bay_bookedTime: [
             {
-              start_time: "2021-09-01 19:00:00",
-              end_time: "2021-09-01 20:00:00",
+              start_time: "2023-10-17T12:00",
+              end_time: "2023-10-17T14:00",
             },
             {
-              start_time: "2021-09-01 20:00:00",
-              end_time: "2021-09-01 21:00:00",
+              start_time: "2023-10-17T18:00",
+              end_time: "2023-10-17T21:00",
             },
           ],
         },
