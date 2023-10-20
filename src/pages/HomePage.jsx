81ad2dd93e75141917ac154d211051b9064b5967
juyeongnoh <juyeongnoh@gmail.com>
2023-10-20 19:29:36 +0900

    feat: 홈화면 API 연동

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,12 @@
+import { Suspense } from "react";
 import HomeTemplate from "../components/templates/HomeTemplate";
 
 const HomePage = () => {
-  const response = {
-    monthlySales: 500000,
-    salesGrowthPercentage: 10.5, // 전월대비 판매 성장률 (단위: %)
-    monthlyReservations: 200,
-    reservationGrowthPercentage: 8.0, // 전월대비 예약 성장률 (단위: %)
-    myStores: [
-      {
-        imageUrl: "https://example.com/carwash1.jpg",
-        name: "세차장 A",
-        monthlySales: 250000,
-        monthlyReservations: 100,
-      },
-      {
-        imageUrl: "https://example.com/carwash2.jpg",
-        name: "세차장 B",
-        monthlySales: 150000,
-        monthlyReservations: 75,
-      },
-    ],
-  };
-
-  return <HomeTemplate home={response} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeTemplate />
+    </Suspense>
+  );
 };
 
 export default HomePage;
