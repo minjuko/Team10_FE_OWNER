4294388ee32f1e9484f3419cf2fa2637173286c6
juyeongnoh <juyeongnoh@gmail.com>
2023-10-20 17:18:59 +0900

    feat: /carwash/ api 생성

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -6,71 +6,92 @@ import { revenue } from "./responses/revenue";
 import { sales } from "./responses/sales";
 
 export const handlers = [
-  // GET
+  // *********** GET ***********
+  // 사장님 홈
   rest.get("/owner/home", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(home));
   }),
 
+  // 매장 관리 - owner 별
   rest.get("/owner/carwashes", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(carwashes));
   }),
 
-  rest.get("/owner/carwashesDetails", (req, res, ctx) => {
+  // 매장 정보 수정(기존 정보 가져오기)
+  rest.get("/owner/carwashes/:carwash_id/details", (req, res, ctx) => {
+    const carwash_id = req.params.carwash_id;
+    // carwash_id로 특정 세차장 가져오는 코드 추가하기
     return res(ctx.status(200), ctx.json(carwashesDetails));
   }),
 
+  // 매출관리 페이지
   rest.get("owner/sales", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200), ctx.json(sales));
     }
   }),
 
+  rest.get("owner/carwashes/:carwash_id", (req, res, ctx) => {
+    const carwash_id = req.params.carwash_id;
+    // carwash_id로 특정 세차장 가져오는 코드 추가하기
+    if (req.body) {
+      return res(ctx.status(200), ctx.json(carwashesDetails));
+    }
+  }),
+
+  // 총 매출 금액 조회
   rest.get("owner/revenue", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200), ctx.json(revenue));
     }
   }),
 
-  // POST
+  // *********** POST ***********
+  // 로그인
   rest.post("/owner/login", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200));
     }
   }),
 
+  // 회원가입
   rest.post("/owner/signup", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200));
     }
   }),
 
-  rest.post("/owner/register", (req, res, ctx) => {
+  // 입점신청
+  rest.post("/owner/carwashes/register", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200));
     }
   }),
 
-  rest.post("/owner/:carwash_id/bays", (req, res, ctx) => {
+  // 베이 추가
+  rest.post("/owner/carwashes/:carwash_id/bays", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200));
     }
   }),
 
-  // PUT
-
+  // *********** PUT ***********
+  // 매장 정보 수정
   rest.put("/owner/carwashes/:carwash_id/details", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200));
     }
   }),
 
+  // 베이 활성화/비활성화
   rest.put("owner/bays/:bay_id/status", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200));
     }
   }),
 
-  // DELETE
+  // *********** DELETE ***********
+  // 예약 취소
   rest.delete("/reservations/:reservation_id", (req, res, ctx) => {
     if (req.body) {
       return res(ctx.status(200));
