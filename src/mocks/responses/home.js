ede7993cf223c214d835d2b246a04159dba10979
juyeongnoh <juyeongnoh@gmail.com>
2023-10-20 16:04:39 +0900

    chore: 개별 응답 파일 생성

---
+++ src/mocks/responses/home.js
@@ -0,0 +1,24 @@
+export const home = {
+  success: true,
+  response: {
+    monthlySales: 500000,
+    salesGrowthPercentage: 10.5, // 전월대비 판매 성장률 (단위: %)
+    monthlyReservations: 200,
+    reservationGrowthPercentage: 8.0, // 전월대비 예약 성장률 (단위: %)
+    myStores: [
+      {
+        imageUrl: "https://example.com/carwash1.jpg",
+        name: "세차장 A",
+        monthlySales: 250000,
+        monthlyReservations: 100,
+      },
+      {
+        imageUrl: "https://example.com/carwash2.jpg",
+        name: "세차장 B",
+        monthlySales: 150000,
+        monthlyReservations: 75,
+      },
+    ],
+  },
+  error: null,
+};
