7e89219fea95e9ba1620ae3adad87e1432edc800
juyeongnoh <juyeongnoh@gmail.com>
2023-10-20 18:32:32 +0900

    chore: react-query에서 @tanstack/react-query로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { QueryClientProvider, QueryClient } from "react-query";
 import { worker } from "./mocks/worker.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 worker.start();
 
