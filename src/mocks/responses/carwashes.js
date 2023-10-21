b60d471a1d85202089ce7c5cbbb1d82f86a2a105
juyeongnoh <juyeongnoh@gmail.com>
2023-10-21 16:55:06 +0900

    feat: 매장관리 페이지 API 연동

--- src/mocks/responses/carwashes.js
+++ src/mocks/responses/carwashes.js
@@ -1,9 +1,18 @@
+const generateToday = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+
+  return `${year}-${month}-${date}`;
+};
+
 export const carwashes = {
   success: true,
   response: {
     carwash: [
       {
-        id: "1",
+        carwash_id: 1,
         image:
           "https://github.com/Step3-kakao-tech-campus/Team10_FE_OWNER/assets/104883910/b214edfd-a3b7-4eb0-aaef-9dd4705ca24e",
         name: "포세이돈워시 용봉점",
@@ -11,13 +20,13 @@ export const carwashes = {
           // 운영시간
           weekday: {
             // 평일
-            start: "09:00:00",
-            end: "17:00:00",
+            start: "00:00:00",
+            end: "23:59:00",
           },
           weekend: {
             // 주말
-            start: "10:00:00",
-            end: "16:00:00",
+            start: "00:00:00",
+            end: "23:59:00",
           },
         },
         bays: [
@@ -25,12 +34,146 @@ export const carwashes = {
             bay_no: "1",
             bay_bookedTime: [
               {
-                start_time: "2023-10-20T10:00",
-                end_time: "2023-10-20T12:00",
+                start_time: `${generateToday()}T10:00`,
+                end_time: `${generateToday()}T13:00`,
               },
               {
-                start_time: "2023-10-20T16:00",
-                end_time: "2023-10-20T18:00",
+                start_time: `${generateToday()}T14:00`,
+                end_time: `${generateToday()}T20:00`,
+              },
+              {
+                start_time: `${generateToday()}T20:00`,
+                end_time: `${generateToday()}T22:00`,
+              },
+              {
+                start_time: `${generateToday()}T22:00`,
+                end_time: `${generateToday()}T23:00`,
+              },
+            ],
+          },
+          {
+            bay_no: "3",
+            bay_bookedTime: [
+              {
+                start_time: `${generateToday()}T10:00`,
+                end_time: `${generateToday()}T13:00`,
+              },
+              {
+                start_time: `${generateToday()}T14:00`,
+                end_time: `${generateToday()}T20:00`,
+              },
+              {
+                start_time: `${generateToday()}T20:00`,
+                end_time: `${generateToday()}T22:00`,
+              },
+              {
+                start_time: `${generateToday()}T22:00`,
+                end_time: `${generateToday()}T23:00`,
+              },
+            ],
+          },
+          {
+            bay_no: "4",
+            bay_bookedTime: [
+              {
+                start_time: `${generateToday()}T10:00`,
+                end_time: `${generateToday()}T13:00`,
+              },
+              {
+                start_time: `${generateToday()}T14:00`,
+                end_time: `${generateToday()}T20:00`,
+              },
+              {
+                start_time: `${generateToday()}T20:00`,
+                end_time: `${generateToday()}T22:00`,
+              },
+              {
+                start_time: `${generateToday()}T22:00`,
+                end_time: `${generateToday()}T23:00`,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        carwash_id: 2,
+        image:
+          "https://github.com/Step3-kakao-tech-campus/Team10_FE_OWNER/assets/104883910/1572934c-920f-4e84-a3d3-dcb4506eea13",
+        name: "대성셀프세차장",
+        optime: {
+          // 운영시간
+          weekday: {
+            // 평일
+            start: "00:00:00",
+            end: "23:59:00",
+          },
+          weekend: {
+            // 주말
+            start: "00:00:00",
+            end: "23:59:00",
+          },
+        },
+        bays: [
+          {
+            bay_no: "1",
+            bay_bookedTime: [
+              {
+                start_time: `${generateToday()}T10:00`,
+                end_time: `${generateToday()}T13:00`,
+              },
+              {
+                start_time: `${generateToday()}T14:00`,
+                end_time: `${generateToday()}T20:00`,
+              },
+              {
+                start_time: `${generateToday()}T20:00`,
+                end_time: `${generateToday()}T22:00`,
+              },
+              {
+                start_time: `${generateToday()}T22:00`,
+                end_time: `${generateToday()}T23:00`,
+              },
+            ],
+          },
+          {
+            bay_no: "3",
+            bay_bookedTime: [
+              {
+                start_time: `${generateToday()}T10:00`,
+                end_time: `${generateToday()}T13:00`,
+              },
+              {
+                start_time: `${generateToday()}T14:00`,
+                end_time: `${generateToday()}T20:00`,
+              },
+              {
+                start_time: `${generateToday()}T20:00`,
+                end_time: `${generateToday()}T22:00`,
+              },
+              {
+                start_time: `${generateToday()}T22:00`,
+                end_time: `${generateToday()}T23:00`,
+              },
+            ],
+          },
+          {
+            bay_no: "4",
+            bay_bookedTime: [
+              {
+                start_time: `${generateToday()}T10:00`,
+                end_time: `${generateToday()}T13:00`,
+              },
+              {
+                start_time: `${generateToday()}T14:00`,
+                end_time: `${generateToday()}T20:00`,
+              },
+              {
+                start_time: `${generateToday()}T20:00`,
+                end_time: `${generateToday()}T22:00`,
+              },
+              {
+                start_time: `${generateToday()}T22:00`,
+                end_time: `${generateToday()}T23:00`,
               },
             ],
           },
