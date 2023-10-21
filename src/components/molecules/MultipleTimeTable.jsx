3a53a7824e71c48dc3e6ce8539d47230eff551be
juyeongnoh <juyeongnoh@gmail.com>
2023-10-21 16:55:06 +0900

    feat: 매장관리 페이지 API 연동

--- src/components/molecules/MultipleTimeTable.jsx
+++ src/components/molecules/MultipleTimeTable.jsx
@@ -1,38 +1,66 @@
-const MultipleTimeTable = ({ carwash }) => {
-  const startTime = new Date(carwash.start_time);
-  const endTime = new Date(carwash.end_time);
+import dayjs from "dayjs";
 
-  let startHour = startTime.getHours();
-  let startMinute = startTime.getMinutes();
+const MultipleTimeTable = ({ carwash }) => {
+  // 오늘이 주말인지 확인하는 함수
+  const isTodayWeekend = () => {
+    const today = dayjs();
+    const day = today.day();
+
+    return day === 0 || day === 6;
+  };
+
+  // 문자열을 dayjs 객체로 변환하는 함수
+  const convertToDayjs = (timeString) => {
+    const [hour, minute] = timeString.split(":").map(Number);
+    return dayjs().startOf("day").hour(hour).minute(minute);
+  };
+
+  // 주말인지 여부에 따라 시간표 시작 시간과 끝 시간을 설정
+  let startTime;
+  let endTime;
+
+  if (isTodayWeekend()) {
+    startTime = convertToDayjs(carwash.optime.weekend.start);
+    endTime = convertToDayjs(carwash.optime.weekend.end);
+  } else {
+    startTime = convertToDayjs(carwash.optime.weekday.start);
+    endTime = convertToDayjs(carwash.optime.weekday.end);
+  }
+
+  // 시간표 시작 시간과 끝 시간을 시간과 분으로 나눔
+  // 분의 경우는 다음과 같이 처리:
+  // 1. 끝 시간이 23:59인 경우, 24:00으로 변경
+  // 2. 끝 시간이 시작 시간보다 뒤인 경우, 끝 시간에 24시간을 더함 (12시 넘어서 운영하는 경우)
+  // 3. 그 외의 경우는 끝 시간 그대로 사용
+  let startHour = startTime.hour();
+  let startMinute = startTime.minute();
   let endHour;
   let endMinute;
 
-  // 24시간 운영할 때: endHour와 endMinute을 24시 0분으로 설정
-  if (endTime.getHours() === 23 && endTime.getMinutes() === 59) {
+  if (endTime.hour() === 23 && endTime.minute() === 59) {
     endHour = 24;
     endMinute = 0;
-  }
-  // 운영시간이 하루를 넘어가는 경우: endHour에 24를 더해서 설정
-  else if (endTime.getDate() > startTime.getDate()) {
-    endHour = endTime.getHours() + 24;
-    endMinute = endTime.getMinutes();
-  }
-  // 운영시간이 하루를 넘어가지 않는 경우: endHour와 endMinute을 그대로 설정
-  else {
-    endHour = endTime.getHours();
-    endMinute = endTime.getMinutes();
+  } else if (endTime.date() > startTime.date()) {
+    endHour = endTime.hour() + 24;
+    endMinute = endTime.minute();
+  } else {
+    endHour = endTime.hour();
+    endMinute = endTime.minute();
   }
 
+  // 시간표에 예약된 시간이 있는지 확인하는 함수
+  // 예약된 시간이 있으면 true, 없으면 false를 반환
   const isTimeSlotBooked = (hour, minute, bookedTime) => {
-    const slotStartTime = new Date(carwash.start_time);
-    slotStartTime.setHours(hour, minute);
-    const slotEndTime = new Date(slotStartTime);
-    slotEndTime.setMinutes(slotStartTime.getMinutes() + 30);
+    const slotStartTime = dayjs(startTime).hour(hour).minute(minute);
+    const slotEndTime = slotStartTime.add(30, "minute");
 
     return bookedTime.some(
       (booking) =>
-        new Date(booking.start_time) <= slotStartTime &&
-        new Date(booking.end_time) >= slotEndTime
+        // dayjs에 <=, >= 연산자가 없어서 isSame, isBefore, isAfter 메서드를 사용
+        (dayjs(booking.start_time).isSame(slotStartTime) ||
+          dayjs(booking.start_time).isBefore(slotStartTime)) &&
+        (dayjs(booking.end_time).isSame(slotEndTime) ||
+          dayjs(booking.end_time).isAfter(slotEndTime))
     );
   };
 
@@ -77,11 +105,11 @@ const MultipleTimeTable = ({ carwash }) => {
     result.push(
       <td
         key={`베이 ${bay_no}`}
-        className={`w-16 h-6 border border-gray-500 text-center
-        }`}>
+        className="w-16 h-6 text-center border border-gray-500">
         베이 {bay_no}
       </td>
     );
+
     while (
       currentHour < endHour ||
       (currentHour === endHour && currentMinute < endMinute)
@@ -90,9 +118,9 @@ const MultipleTimeTable = ({ carwash }) => {
 
       result.push(
         <td
-          key={`${currentHour}-${currentMinute}`}
+          key={`${currentHour}:${currentMinute}`}
           className={`w-5 h-8 border border-gray-500 ${
-            isBooked ? "bg-primary hover:bg-sky-300" : ""
+            isBooked && "bg-primary"
           }`}></td>
       );
 
@@ -113,7 +141,7 @@ const MultipleTimeTable = ({ carwash }) => {
         <tr className="bg-gray-300">{hourLoop(startHour, endHour)}</tr>
       </thead>
       <tbody>
-        {carwash.bay_list.map((item) => {
+        {carwash.bays.map((item) => {
           return (
             <tr key={item.bay_no}>
               {minuteLoop(
--- src/pages/CarwashManagementPage.jsx
+++ src/pages/CarwashManagementPage.jsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import CarwashManagementTemplate from "../components/templates/CarwashManagementTemplate";
 
 const CarwashManagementPage = () => {
-  return <CarwashManagementTemplate />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CarwashManagementTemplate />
+    </Suspense>
+  );
 };
 
 export default CarwashManagementPage;
