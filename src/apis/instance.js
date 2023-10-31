e8279d161ed0017c9ad7fb2622860f3c736eb08c
juyeongnoh <juyeongnoh@gmail.com>
2023-11-01 02:23:04 +0900

    feat: redux-thunk로 로그인 상태 관리
    로그인 성공 시 사용자 이메일을 전역 상태로 관리
    
    resolves: #74

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const token = localStorage.getItem("Token");
+
 export const instance = axios.create({
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
+    authorization: token,
   },
 });
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import "./index.css";
 import { worker } from "./mocks/worker.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
+
+import store from "./store/store.js";
 
 worker.start();
 
@@ -12,9 +15,11 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
