554cf4eaaf28ce9c3278f8be27c2a6fe2c01dcaa
juyeongnoh <juyeongnoh@gmail.com>
2023-10-31 22:52:05 +0900

    chore: axios 모듈화 API 비동기 함수로 변경

--- src/apis/carwashes.js
+++ src/apis/carwashes.js
@@ -1,25 +1,25 @@
 import { instance } from "./instance";
 
-export const getCarwashes = () => {
+export const getCarwashes = async () => {
   return instance.get("/owner/carwashes");
 };
 
-export const getCarwashesDetails = (carwash_id) => {
+export const getCarwashesDetails = async (carwash_id) => {
   return instance.get(`/owner/carwashes/${carwash_id}/details`);
 };
 
-export const putCarwashesDetails = (carwash_id, data) => {
+export const putCarwashesDetails = async (carwash_id, data) => {
   return instance.put(`/owner/carwashes/${carwash_id}/details`, data);
 };
 
-export const register = (data) => {
+export const register = async (data) => {
   return instance.post("/owner/carwashes/register", data);
 };
 
-export const addBays = (carwash_id, bay_number) => {
+export const addBays = async (carwash_id, bay_number) => {
   return instance.post(`/owner/carwashes/${carwash_id}}/bays`, bay_number);
 };
 
-export const getCarwashItem = (carwash_id) => {
+export const getCarwashItem = async (carwash_id) => {
   return instance.get(`/owner/carwashes/${carwash_id}`);
 };
--- src/apis/extras.js
+++ src/apis/extras.js
@@ -1,24 +1,24 @@
 import { instance } from "./instance";
 
 // 홈화면 데이터
-export const getHome = () => {
+export const getHome = async () => {
   return instance.get("/owner/home");
 };
 
 // 베이 활성화 비활성화
-export const setBayStatus = (bay_id, status) => {
+export const setBayStatus = async (bay_id, status) => {
   return instance.get(`/owner/bays/${bay_id}}/status`, { status });
 };
 
 // 매출관리 데이터
-export const getSales = (carwash_id, selected_date) => {
+export const getSales = async (carwash_id, selected_date) => {
   return instance.get(
     `/owner/sales?carwash-id=${carwash_id}&selected-date=${selected_date}`
   );
 };
 
 // 월 매출 데이터
-export const getRevenue = (carwash_id, selected_date) => {
+export const getRevenue = async (carwash_id, selected_date) => {
   return instance.get(
     `/owner/revenue?carwash-id=${carwash_id}&selected-date=${selected_date}`
   );
