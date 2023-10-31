4004a0242b8c4a639ff9461e072860d844c56a21
juyeongnoh <juyeongnoh@gmail.com>
2023-11-01 03:26:47 +0900

    fix: CarwashItem 안에 내용이 많아지면 사진 배치 어색해지는 현상 수정
    resolves: #58

--- src/components/organisms/CarwashItem.jsx
+++ src/components/organisms/CarwashItem.jsx
@@ -4,7 +4,13 @@ import MultipleTimeTable from "../molecules/MultipleTimeTable";
 const CarwashItem = ({ carwash }) => {
   return (
     <Link to="/manage/item" className="flex overflow-auto shadow-xl rounded-xl">
-      <img className="w-48 h-48" src={carwash.image} alt={carwash.name} />
+      <div className="relative w-48">
+        <img
+          className="absolute object-cover w-full h-full"
+          src={carwash.image}
+          alt={carwash.name}
+        />
+      </div>
       <div className="flex-grow">
         <div className="p-4 text-2xl font-bold text-white bg-primary">
           {carwash.name}
