27136fdbf7c8aa88276f15084a2fcbf42278125d
juyeongnoh <juyeongnoh@gmail.com>
2023-11-04 01:18:53 +0900

    chore: axios instance baseUrl

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -4,8 +4,17 @@ const token = localStorage.getItem("Token");
 
 export const instance = axios.create({
   timeout: 5000,
+  baseURL: "https://k29e4c1a30136a.user-app.krampoline.com",
   headers: {
     "Content-Type": "application/json",
     authorization: token,
   },
 });
+
+// export const instance = axios.create({
+//   timeout: 5000,
+//   headers: {
+//     "Content-Type": "application/json",
+//     authorization: token,
+//   },
+// });
