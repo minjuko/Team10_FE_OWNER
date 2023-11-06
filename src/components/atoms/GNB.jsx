49f37c5948f73ca57e2cdbd7d735bfcd145cc658
juyeongnoh <juyeongnoh@gmail.com>
2023-11-06 11:12:16 +0900

    design: GNB 흰 배경 꽉차게, 메뉴 클릭시 레이아웃 깨지는 현상 수정

--- src/components/atoms/GNB.jsx
+++ src/components/atoms/GNB.jsx
@@ -5,6 +5,14 @@ import Button from "./Button";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../store/slices/authSlice";
 
+/**
+ * GNB 컴포넌트
+ *
+ * 사장님 사이트에서 전역적으로 상단에 표시되는 네비게이션 바입니다.
+ *
+ * @todo 로그아웃 버튼 클릭 시 로그아웃 처리\
+ */
+
 const GNB = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -24,49 +32,48 @@ const GNB = () => {
 
   return (
     <nav className="border-b border-gray-300">
-      <div className="w-[1280px] mx-auto flex justify-between items-center px-4 bg-white">
-        <section className="flex items-center gap-12">
-          <Link to="/">
-            <Image src={Logo} alt="뽀득뽀득 사장님 페이지 로고" />
-          </Link>
+      <div className="bg-white">
+        <div className="w-[1280px] flex items-center justify-between mx-auto px-4">
+          <section className="flex items-center gap-4">
+            <Link to="/">
+              <Image src={Logo} alt="뽀득뽀득 사장님 페이지 로고" />
+            </Link>
 
-          <ul className="flex">
-            {menus.map((menu, index) => {
-              return (
-                <Link key={index} to={menu.path}>
-                  <li
-                    className={`${
-                      location.pathname === menu.path &&
-                      "border-b-8 border-primary text-primary"
-                    } text-xl p-8`}>
-                    {menu.label}
-                  </li>
-                </Link>
-              );
-            })}
-          </ul>
-        </section>
+            {menus.map((menu, index) => (
+              <Link
+                className={`text-xl p-8 ${
+                  location.pathname === menu.path
+                    ? "border-b-8 border-primary text-primary"
+                    : "border-b-8 border-white"
+                } `}
+                key={index}
+                to={menu.path}>
+                {menu.label}
+              </Link>
+            ))}
+          </section>
 
-        <section className="flex gap-6">
-          <div className="text-right">
-            <div className="text-xl">{userName} 사장님</div>
+          <section className="flex gap-6">
+            <div className="text-right">
+              <div className="text-xl">{userName} 사장님</div>
+              <Button
+                className="text-gray-500"
+                onClick={() => {
+                  dispatch(logout());
+                  navigate("/login");
+                }}>
+                로그아웃
+              </Button>
+            </div>
             <Button
-              className="text-gray-500"
+              style="cta"
               onClick={() => {
-                dispatch(logout());
-                navigate("/login");
+                navigate("/register");
               }}>
-              로그아웃
+              입점하기
             </Button>
-          </div>
-          <Button
-            style="cta"
-            onClick={() => {
-              navigate("/register");
-            }}>
-            입점하기
-          </Button>
-        </section>
+          </section>
+        </div>
       </div>
     </nav>
   );
