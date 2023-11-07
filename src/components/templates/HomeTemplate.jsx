60fb0e207eeac54af15659a62ecc612c9e6c55a1
juyeongnoh <juyeongnoh@gmail.com>
2023-11-08 07:29:28 +0900

    design: 사용되지 않는 div 태그 제거

--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -11,39 +11,33 @@ const HomeTemplate = () => {
   const userName = useSelector((state) => state.auth.userName);
 
   return (
-    <div>
-      {data && (
-        <div className="grid gap-8">
-          <section className="grid gap-4">
-            <h1 className="text-3xl font-bold">
-              {userName} 사장님 안녕하세요!
-            </h1>
-            <div className="flex gap-12">
-              <DashboardItem
-                label="이번 달 전체 판매"
-                icon={SalesIcon}
-                number={data.data.response.monthlySales}
-                percentage={data.data.response.salesGrowthPercentage}
-              />
-              <DashboardItem
-                label="이번 달 전체 예약"
-                icon={ReservationIcon}
-                number={data.data.response.monthlyReservations}
-                percentage={data.data.response.reservationGrowthPercentage}
-              />
-            </div>
-          </section>
-
-          <section className="grid gap-4">
-            <h2 className="text-2xl font-bold">{userName} 사장님의 매장</h2>
-            <div className="flex flex-wrap gap-12">
-              {data.data.response.myStores.map((store) => (
-                <CarwashShortcutItem key={store.name} carwash={store} />
-              ))}
-            </div>
-          </section>
+    <div className="grid gap-8">
+      <section className="grid gap-4">
+        <h1 className="text-3xl font-bold">{userName} 사장님 안녕하세요!</h1>
+        <div className="flex gap-12">
+          <DashboardItem
+            label="이번 달 전체 판매"
+            icon={SalesIcon}
+            number={data.data.response.monthlySales}
+            percentage={data.data.response.salesGrowthPercentage}
+          />
+          <DashboardItem
+            label="이번 달 전체 예약"
+            icon={ReservationIcon}
+            number={data.data.response.monthlyReservations}
+            percentage={data.data.response.reservationGrowthPercentage}
+          />
         </div>
-      )}
+      </section>
+
+      <section className="grid gap-4">
+        <h2 className="text-2xl font-bold">{userName} 사장님의 매장</h2>
+        <div className="flex flex-wrap gap-12">
+          {data.data.response.myStores.map((store) => (
+            <CarwashShortcutItem key={store.name} carwash={store} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
