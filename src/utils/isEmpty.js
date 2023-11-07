9d1f8ada8c8ad354f553ce1e6328e847b897ec84
juyeongnoh <juyeongnoh@gmail.com>
2023-11-08 02:28:36 +0900

    chore: 매장관리 페이지에서 표시할 매장이나 베이가 없을 때 메시지 표시

---
+++ src/utils/isEmpty.js
@@ -0,0 +1,12 @@
+/**
+ * isEmpty
+ *
+ * 배열이 비었는지 검사하는 함수입니다.
+ *
+ * @param {Array} array 비었는지 검사할 배열
+ * @returns {Boolean} 비었으면 true, 아니면 false
+ */
+export const isEmpty = (array) => {
+  if (array.length === 0) return true;
+  else return false;
+};
