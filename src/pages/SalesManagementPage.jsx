114fda0472fd2163140c9177d22025e49f77258b
juyeongnoh <juyeongnoh@gmail.com>
2023-11-10 01:06:59 +0900

    feat: LoadingAnimation Suspense 사용 페이지에 적용

--- src/pages/SalesManagementPage.jsx
+++ src/pages/SalesManagementPage.jsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import SalesManagementTemplate from "../components/templates/SalesManagementTemplate";
 import { ErrorBoundary } from "react-error-boundary";
+import LoadingAnimation from "../components/atoms/LoadingAnimation";
 
 const SalesManagementPage = () => {
   return (
     <ErrorBoundary fallback={<div>Error occurred!</div>}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<LoadingAnimation />}>
         <SalesManagementTemplate />
       </Suspense>
     </ErrorBoundary>
