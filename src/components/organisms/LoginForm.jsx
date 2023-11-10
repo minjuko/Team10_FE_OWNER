1cfa34aa582eabdcff64fe10fc7cca67f88b4322
juyeongnoh <juyeongnoh@gmail.com>
2023-11-11 00:23:12 +0900

    feat: 로그인 에러 코드에 따라 메시지 표시

--- src/components/organisms/LoginForm.jsx
+++ src/components/organisms/LoginForm.jsx
@@ -35,7 +35,28 @@ const LoginForm = () => {
         navigate("/");
       })
       .catch((error) => {
-        setErrorMessage(error.error.message);
+        let message;
+
+        if (error.error.status === "400") {
+          switch (error.error.code) {
+            case "1001":
+              message = "이메일 형식에 맞게 입력해주세요.";
+              break;
+            default:
+              message = "오류가 발생했습니다. 잠시 후 다시 시도해 주세요.";
+              break;
+          }
+        } else if (error.error.status === "401") {
+          switch (error.error.code) {
+            case "1201":
+              message = "비밀번호가 일치하지 않습니다.";
+              break;
+            default:
+              message = "오류가 발생했습니다. 잠시 후 다시 시도해 주세요.";
+              break;
+          }
+        }
+        setErrorMessage(message);
       });
   };
 
