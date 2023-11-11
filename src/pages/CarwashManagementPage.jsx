bdf49896a835319d0b11d503281ed1a79a378b08
juyeongnoh <juyeongnoh@gmail.com>
2023-11-11 12:10:25 +0900

    feat: 매장관리 페이지 오류처리

--- src/pages/CarwashManagementPage.jsx
+++ src/pages/CarwashManagementPage.jsx
@@ -3,11 +3,36 @@ import CarwashManagementTemplate from "../components/templates/CarwashManagement
 import { ErrorBoundary } from "react-error-boundary";
 import LoadingAnimation from "../components/atoms/LoadingAnimation";
 
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  const errorCode = error.response.data.error.code;
+
+  switch (errorCode) {
+    case "1201":
+      return (
+        <FallbackLayout
+          message="로그인이 필요한 페이지입니다."
+          buttonLabel="로그인"
+          to="/login"
+        />
+      );
+    default:
+      return (
+        <FallbackLayout
+          message="알 수 없는 오류가 발생했습니다."
+          buttonLabel="홈으로"
+          to="/"
+        />
+      );
+  }
+};
+
 const CarwashManagementPage = () => {
   return (
-    <Suspense fallback={<LoadingAnimation />}>
-      <CarwashManagementTemplate />
-    </Suspense>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Suspense fallback={<LoadingAnimation />}>
+        <CarwashManagementTemplate />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
