b973573f97d485793df932058defb2db6652c464
고민주 <83636444+minjuko@users.noreply.github.com>
2026-09-13 01:42:05 +0900

    fix: align Vite React plugin dependency

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd());
